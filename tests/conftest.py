dc77d3afe0b7fb44c33eb1771878eda1ceb8e227
Fernandezl7 <fernandezlizbeth5@gmail.com>
2025-02-18 20:39:49 -0500

    added faker to requirements and tested

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
+'''Test CONF file'''
+# pylint: disable=line-too-long,unused-argument, comparison-with-callable
 # conftest.py
-import pytest
+#import pytest
 from decimal import Decimal
 from faker import Faker
 from calculator.operations import add, subtract, multiply, divide
@@ -7,6 +9,7 @@ from calculator.operations import add, subtract, multiply, divide
 fake = Faker()
 
 def generate_test_data(num_records):
+    '''generate test data function'''
     # Define operation mappings for both Calculator and Calculation tests
     operation_mappings = {
         'add': add,
@@ -20,11 +23,9 @@ def generate_test_data(num_records):
         b = Decimal(fake.random_number(digits=2)) if _ % 4 != 3 else Decimal(fake.random_number(digits=1))
         operation_name = fake.random_element(elements=list(operation_mappings.keys()))
         operation_func = operation_mappings[operation_name]
-        
         # Ensure b is not zero for divide operation to prevent division by zero in expected calculation
         if operation_func == divide:
             b = Decimal('1') if b == Decimal('0') else b
-        
         try:
             if operation_func == divide and b == Decimal('0'):
                 expected = "ZeroDivisionError"
@@ -32,13 +33,14 @@ def generate_test_data(num_records):
                 expected = operation_func(a, b)
         except ZeroDivisionError:
             expected = "ZeroDivisionError"
-        
         yield a, b, operation_name, operation_func, expected
 
 def pytest_addoption(parser):
+    '''pytest_adoption function'''
     parser.addoption("--num_records", action="store", default=5, type=int, help="Number of test records to generate")
 
 def pytest_generate_tests(metafunc):
+    '''pytest_generate_tests'''
     # Check if the test is expecting any of the dynamically generated fixtures
     if {"a", "b", "expected"}.intersection(set(metafunc.fixturenames)):
         num_records = metafunc.config.getoption("num_records")
@@ -49,4 +51,3 @@ def pytest_generate_tests(metafunc):
         # Modify parameters to fit test functions' expectations
         modified_parameters = [(a, b, op_name if 'operation_name' in metafunc.fixturenames else op_func, expected) for a, b, op_name, op_func, expected in parameters]
         metafunc.parametrize("a,b,operation,expected", modified_parameters)
-        
